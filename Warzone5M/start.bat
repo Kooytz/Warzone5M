1cc2a8a4e04818c63359ff398594d19e89a344db
Kooytz <92762021+Kooytz@users.noreply.github.com>
2022-11-03 15:11:41 -0300

    Update start.bat

--- Warzone5M/start.bat
+++ Warzone5M/start.bat
@@ -1,1 +1,1 @@
-start ..\artefatos\FXServer.exe +set onesync on +set onesync_population true +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +exec Warzone5M.cfg
+start ..\build\FXServer.exe +set onesync on +set onesync_population true +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +exec Warzone5M.cfg
